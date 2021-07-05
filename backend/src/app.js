bafa5b7f0955005bc5edacee1b70d0f399257497
juanilloDG <dual.juan@gmail.com>
2021-07-05 15:57:17 +0200

    Enable cors policy and unused code removed

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,8 +10,12 @@ mongoose.connect("mongodb://localhost/news", { useNewUrlParser: true, useUnified
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-var models = require('./models/news');
 var dataDB = require('./controllers/dataDb.controllers');
 
 //Routes
